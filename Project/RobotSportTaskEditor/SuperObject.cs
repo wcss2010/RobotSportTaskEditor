1241eaab33e4ffddc21943d0127f322118dbe85c
wcss2010 <wcss2020@gmail.com>
2018-07-16 00:16:51 +0800

    .......

--- Project/RobotSportTaskEditor/SuperObject.cs
+++ Project/RobotSportTaskEditor/SuperObject.cs
@@ -16,9 +16,9 @@ namespace RobotSportTaskEditor
             control.RevolveAngleLimitDict.Add(0, new RevolveAngleLimit(-55, +55));
             control.RevolveAngleLimitDict.Add(1, new RevolveAngleLimit(0, +60));
             control.RevolveAngleLimitDict.Add(2, new RevolveAngleLimit(-55, +55));
-            control.RevolveAngleLimitDict.Add(3, new RevolveAngleLimit(0, -60));
+            control.RevolveAngleLimitDict.Add(3, new RevolveAngleLimit(-60, 0));
             control.RevolveAngleLimitDict.Add(4, new RevolveAngleLimit(-55, +55));
-            control.RevolveAngleLimitDict.Add(5, new RevolveAngleLimit(0, -60));
+            control.RevolveAngleLimitDict.Add(5, new RevolveAngleLimit(-60, 0));
             control.RevolveAngleLimitDict.Add(6, new RevolveAngleLimit(-55, +55));
             control.RevolveAngleLimitDict.Add(7, new RevolveAngleLimit(0, +60));
             control.RevolveAngleLimitDict.Add(8, new RevolveAngleLimit(-55, +55));

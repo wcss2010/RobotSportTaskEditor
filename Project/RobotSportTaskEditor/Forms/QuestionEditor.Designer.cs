cc55d6df5aa22cfb9d0dfcbbae852f7f437381e2
wcss2010 <wcss2020@gmail.com>
2018-07-15 16:38:01 +0800

    .....

--- Project/RobotSportTaskEditor/Forms/QuestionEditor.Designer.cs
+++ Project/RobotSportTaskEditor/Forms/QuestionEditor.Designer.cs
@@ -29,22 +29,71 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tbAsk = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.btnOK = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
+            this.tbAnswer = new System.Windows.Forms.TextBox();
+            this.cbActions = new System.Windows.Forms.ComboBox();
+            this.btnNewAction = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.btnNewAction);
+            this.groupBox1.Controls.Add(this.cbActions);
+            this.groupBox1.Controls.Add(this.tbAnswer);
+            this.groupBox1.Controls.Add(this.tbAsk);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Location = new System.Drawing.Point(2, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(508, 161);
+            this.groupBox1.Size = new System.Drawing.Size(508, 249);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
+            // 
+            // tbAsk
+            // 
+            this.tbAsk.Location = new System.Drawing.Point(64, 16);
+            this.tbAsk.Multiline = true;
+            this.tbAsk.Name = "tbAsk";
+            this.tbAsk.Size = new System.Drawing.Size(434, 96);
+            this.tbAsk.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 223);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "动作：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 162);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "答：";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "问：";
             // 
             // btnOK
             // 
-            this.btnOK.Location = new System.Drawing.Point(99, 169);
+            this.btnOK.Location = new System.Drawing.Point(88, 257);
             this.btnOK.Name = "btnOK";
             this.btnOK.Size = new System.Drawing.Size(83, 34);
             this.btnOK.TabIndex = 1;
@@ -54,7 +103,7 @@
             // 
             // btnCancel
             // 
-            this.btnCancel.Location = new System.Drawing.Point(342, 169);
+            this.btnCancel.Location = new System.Drawing.Point(331, 257);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(83, 34);
             this.btnCancel.TabIndex = 1;
@@ -62,21 +111,51 @@
             this.btnCancel.UseVisualStyleBackColor = true;
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
+            // tbAnswer
+            // 
+            this.tbAnswer.Location = new System.Drawing.Point(64, 118);
+            this.tbAnswer.Multiline = true;
+            this.tbAnswer.Name = "tbAnswer";
+            this.tbAnswer.Size = new System.Drawing.Size(434, 96);
+            this.tbAnswer.TabIndex = 1;
+            // 
+            // cbActions
+            // 
+            this.cbActions.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbActions.FormattingEnabled = true;
+            this.cbActions.Location = new System.Drawing.Point(64, 220);
+            this.cbActions.Name = "cbActions";
+            this.cbActions.Size = new System.Drawing.Size(340, 20);
+            this.cbActions.TabIndex = 2;
+            // 
+            // btnNewAction
+            // 
+            this.btnNewAction.Location = new System.Drawing.Point(410, 218);
+            this.btnNewAction.Name = "btnNewAction";
+            this.btnNewAction.Size = new System.Drawing.Size(88, 23);
+            this.btnNewAction.TabIndex = 3;
+            this.btnNewAction.Text = "新建动作";
+            this.btnNewAction.UseVisualStyleBackColor = true;
+            this.btnNewAction.Click += new System.EventHandler(this.btnNewAction_Click);
+            // 
             // QuestionEditor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(512, 207);
+            this.ClientSize = new System.Drawing.Size(512, 298);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnOK);
             this.Controls.Add(this.groupBox1);
             this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "QuestionEditor";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "编辑问答";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -86,5 +165,12 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button btnOK;
         private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.TextBox tbAsk;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbAnswer;
+        private System.Windows.Forms.ComboBox cbActions;
+        private System.Windows.Forms.Button btnNewAction;
     }
 }
--- Project/RobotSportTaskEditor/Forms/QuestionEditor.cs
+++ Project/RobotSportTaskEditor/Forms/QuestionEditor.cs
@@ -34,7 +34,37 @@ namespace RobotSportTaskEditor.Forms
         {
             base.OnLoad(e);
 
+            LoadActions();
+            if (Object != null)
+            {
+                tbAsk.Text = Object.Ask;
+                tbAnswer.Text = Object.Answer;
 
+                if (ActionList != null)
+                {
+                    foreach (Robot_Actions action in ActionList)
+                    {
+                        if (action.Id == Object.ActionId)
+                        {
+                            cbActions.SelectedItem = action.Name;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void LoadActions()
+        {
+            ActionList = DBInstance.DbHelper.table("Robot_Actions").select("*").getList<Robot_Actions>(new Robot_Actions());
+            if (ActionList != null)
+            {
+                cbActions.Items.Clear();
+                foreach (Robot_Actions a in ActionList)
+                {
+                    cbActions.Items.Add(a.Name);
+                }
+            }
         }
 
         public bool IsNewRecord { get; set; }
@@ -57,7 +87,52 @@ namespace RobotSportTaskEditor.Forms
 
         private void btnOK_Click(object sender, EventArgs e)
         {
+            if (string.IsNullOrEmpty(tbAsk.Text))
+            {
+                MessageBox.Show("请输入问内容！");
+                return;
+            }
+            if (string.IsNullOrEmpty(tbAnswer.Text))
+            {
+                MessageBox.Show("请输入答内容！");
+                return;
+            }
 
+            if (IsNewRecord)
+            {
+                _object = new Robot_Questions();
+                Object.Id = DBInstance.DbHelper.table("Robot_Questions").select("max(Id)").getValue<long>(0) + 1;
+            }
+
+            Object.Ask = tbAsk.Text;
+            Object.Answer = tbAnswer.Text;
+
+            if (cbActions.SelectedIndex >= 0 && ActionList != null && ActionList.Count > cbActions.SelectedIndex)
+            {
+                Object.ActionId = ActionList[cbActions.SelectedIndex].Id;
+            }
+
+            if (IsNewRecord)
+            {
+                //Insert
+                DBInstance.DbHelper.table("Robot_Questions").set("Id", Object.Id).set("Ask", Object.Ask).set("Answer", Object.Answer).set("ActionId", Object.ActionId).insert();
+            }
+            else
+            {
+                //Update
+                DBInstance.DbHelper.table("Robot_Questions").set("Ask", Object.Ask).set("Answer", Object.Answer).set("ActionId", Object.ActionId).where("Id=?", new object[] { Object.Id }).update();
+            }
+
+            MessageBox.Show("操作完成！");
+            Close();
         }
+
+        private void btnNewAction_Click(object sender, EventArgs e)
+        {
+            new ActionEditor(true).ShowDialog();
+            LoadActions();
+        }
+
+        public List<Robot_Actions> ActionList { get; set; }
     }
 }
